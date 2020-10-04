ffc157a22f8e0339c39aa004a3c7f2dca9dced13
topias t <topias.tallberg@gmail.com>
2020-10-04 06:29:32 +0300

    userslog db works but doesnt not have any functional opetarions yet

--- app.py
+++ app.py
@@ -10,11 +10,13 @@ app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/add",methods=["POST"])
+
+@app.route("/add", methods=["POST"])
 def add():
     paikka = request.form["paikka"]
     liike = request.form["liike"]
@@ -24,42 +26,56 @@ def add():
 
     return redirect("/")
 
-@app.route("/login",methods=["POST"])
+
+@app.route("/login", methods=["POST"])
 def login():
     username = request.form["username"]
     password = request.form["password"]
     sql = "SELECT password FROM users WHERE username=:username"
-    result = db.session.execute(sql, {"username":username})
+    result = db.session.execute(sql, {"username": username})
     user = result.fetchone()
 
     if user == None:
         return render_template("usernameNone.html")
     else:
         hash_value = user[0]
-    if check_password_hash(hash_value,password):
+    if check_password_hash(hash_value, password):
         session["username"] = username
+
+        """ tallennetaan muistiin kirjautumishetki ja käyttäjän id """
+        sql = "SELECT id FROM users WHERE username=:username"
+        result = db.session.execute(sql, {"username": username})
+        usernameid = result.fetchone()[0]
+        sql = "INSERT INTO userlog (usernameid, sent_at) VALUES (:userid, NOW())"
+        db.session.execute(sql, {"userid": usernameid})
+        db.session.commit()
+
         return redirect("/")
     else:
         return render_template("usernameNone.html")
 
-@app.route("/newUser",methods=["POST"])
+
+@app.route("/newUser", methods=["POST"])
 def newUser():
     username = request.form["username"]
     password = request.form["password"]
     sql = "INSERT INTO users (username,password) VALUES (:username,:password)"
     hash_value = generate_password_hash(password)
-    db.session.execute(sql, {"username":username,"password":hash_value})
+    db.session.execute(sql, {"username": username, "password": hash_value})
     db.session.commit()
     return redirect("/")
+
 
 @app.route("/logout")
 def logout():
     del session["username"]
     return redirect("/")
 
+
 @app.route("/userlog")
 def userlog():
     return render_template("userlog.html")
+
 
 @app.route("/statistics")
 def statistics():
--- schema.sql
+++ schema.sql
@@ -1,1 +1,2 @@
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT);
+CREATE TABLE userlog (id SERIAL PRIMARY KEY, usernameid INTEGER, sent_at TIMESTAMP);
