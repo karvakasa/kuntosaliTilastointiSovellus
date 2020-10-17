a9ca96f12f667a1e151d0ec7b3823f696a3ce7b4
topias t <topias.tallberg@gmail.com>
2020-10-18 01:25:30 +0300

    better statistics

--- app.py
+++ app.py
@@ -121,3 +121,60 @@ def statistics():
     stats = result.fetchall()
     
     return render_template("statistics.html", stats=stats)
+
+@app.route("/scottish_bench")
+def scottish_bench():
+
+    username = session.get("username")
+    sql = "SELECT id FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username": username})
+    username_id = result.fetchone()[0]
+    sql = "SELECT date, do_amount, weight_amount, gym_place FROM userstats INNER JOIN gym ON userstats.gym_id=gym.id INNER JOIN equipment ON userstats.equipment_id=equipment.id WHERE users_id=:users_id AND equipment.id=2 ORDER BY date DESC"
+    result = db.session.execute(sql, {"users_id":username_id})
+    stats = result.fetchall()
+
+    return render_template("scottish_bench.html", stats=stats)
+    
+@app.route("/deadlift")
+def deadlift():
+    username = session.get("username")
+    sql = "SELECT id FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username": username})
+    username_id = result.fetchone()[0]
+    sql = "SELECT date, do_amount, weight_amount, gym_place FROM userstats INNER JOIN gym ON userstats.gym_id=gym.id INNER JOIN equipment ON userstats.equipment_id=equipment.id WHERE users_id=:users_id AND equipment.id=5 ORDER BY date DESC"
+    result = db.session.execute(sql, {"users_id":username_id})
+    stats = result.fetchall()
+    return render_template("deadlift.html", stats=stats)
+
+@app.route("/squat")
+def squat():
+    username = session.get("username")
+    sql = "SELECT id FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username": username})
+    username_id = result.fetchone()[0]
+    sql = "SELECT date, do_amount, weight_amount, gym_place FROM userstats INNER JOIN gym ON userstats.gym_id=gym.id INNER JOIN equipment ON userstats.equipment_id=equipment.id WHERE users_id=:users_id AND equipment.id=3 ORDER BY date DESC"
+    result = db.session.execute(sql, {"users_id":username_id})
+    stats = result.fetchall()
+    return render_template("squat.html", stats=stats)
+
+@app.route("/footpress")
+def footpress():
+    username = session.get("username")
+    sql = "SELECT id FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username": username})
+    username_id = result.fetchone()[0]
+    sql = "SELECT date, do_amount, weight_amount, gym_place FROM userstats INNER JOIN gym ON userstats.gym_id=gym.id INNER JOIN equipment ON userstats.equipment_id=equipment.id WHERE users_id=:users_id AND equipment.id=4 ORDER BY date DESC"
+    result = db.session.execute(sql, {"users_id":username_id})
+    stats = result.fetchall()
+    return render_template("footpress.html", stats=stats)
+
+@app.route("/bench_press")
+def bench_press():
+    username = session.get("username")
+    sql = "SELECT id FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username": username})
+    username_id = result.fetchone()[0]
+    sql = "SELECT date, do_amount, weight_amount, gym_place FROM userstats INNER JOIN gym ON userstats.gym_id=gym.id INNER JOIN equipment ON userstats.equipment_id=equipment.id WHERE users_id=:users_id AND equipment.id=1 ORDER BY date DESC"
+    result = db.session.execute(sql, {"users_id":username_id})
+    stats = result.fetchall()
+    return render_template("bench_press.html", stats=stats)
