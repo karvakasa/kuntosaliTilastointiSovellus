f639d434e642f07daee380f96bcef11dbdf90b52
Karvakasa <topias.tallberg@gmail.com>
2021-05-20 17:39:41 +0300

    testicommit

--- users.py
+++ users.py
@@ -24,7 +24,7 @@ def login():
     else:
         hash_value = user[0]
     if check_password_hash(hash_value, password):
-        session["username"] = username
+        asd
 
         """ save log in time and user id """
         sql = "SELECT id FROM users WHERE username=:username"
